c1392156508ec8428653be6ce9ed2c6dfd162fcd
Gerr <german@arriendas.cl>
2017-09-17 19:55:30 -0300

    flask change host

--- app/app.py
+++ app/app.py
@@ -238,10 +238,13 @@ def predict():
 		return response	
 	
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 	#decide what port to run the app in
-	port = int(os.environ.get('PORT', 5000))
+	#port = int(os.environ.get('PORT', 5000))
 	#run the app locally on the givn port
-	app.run(host='localhost', port=port)
+	#app.run(host='localhost', port=port)
 	#optional if we want to run in debugging mode
 	#app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
