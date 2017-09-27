d6a1f95c7f625ef882bb6d9f244635df5969e444
Gerr <german@arriendas.cl>
2017-09-26 23:26:02 -0300

    server side auth

--- app/app.py
+++ app/app.py
@@ -85,8 +85,17 @@ def product():
 #######################
 @app.route('/1112/prediktor/real_time')
 def real_time():
-  return render_template("real_time.html", brand = brand)
-
+    # Google Analytics API autentification
+    from oauth2client.service_account import ServiceAccountCredentials
+    # The scope for the OAuth2 request.
+    SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+    # The location of the key file with the key data.
+    KEY_FILEPATH = 'templates/neem.json'
+    # Defines a method to get an access token from the ServiceAccount object.
+    def get_access_token():
+        ServiceAccountCredentials.from_json_keyfile_name(KEY_FILEPATH, SCOPE).get_access_token().access_token
+    tojen = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILEPATH, SCOPE).get_access_token().access_token
+    return render_template("real_time.html", brand = brand, tojen = tojen)
 
 #######################
 #    HISTORIC VIEW    #
